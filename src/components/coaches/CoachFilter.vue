0603a73be722f28897bb8ef61945a33bca817109
Fabien <cacrfabfr@gmail.com>
2021-10-05 13:07:43 +0200

    added filtered coach component

---
+++ src/components/coaches/CoachFilter.vue
@@ -0,0 +1,68 @@
+<template>
+  <base-card>
+    <h2>Find Your Coach</h2>
+    <span class="filter-option">
+      <input type="checkbox" id="frontend" checked @change="setFilters" />
+      <label for="frontend">Frontend</label>
+    </span>
+    <span class="filter-option">
+      <input type="checkbox" id="backend" checked @change="setFilters" />
+      <label for="backend">Backend</label>
+    </span>
+    <span class="filter-option">
+      <input type="checkbox" id="career" checked @change="setFilters" />
+      <label for="career">Career</label>
+    </span>
+  </base-card>
+</template>
+
+<script>
+export default {
+  emits: ['change-filter'],
+  data() {
+    return {
+      filters: {
+        frontend: true,
+        backend: true,
+        career: true,
+      },
+    };
+  },
+  methods: {
+    setFilters(event) {
+      const inputId = event.target.id;
+      const isActive = event.target.checked;
+      const updatedFilters = {
+        ...this.filters,
+        [inputId]: isActive,
+      };
+      this.filters = updatedFilters;
+      this.$emit('change-filter', updatedFilters);
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+h2 {
+  margin: 0.5rem 0;
+}
+
+.filter-option {
+  margin-right: 1rem;
+
+  label,
+  input {
+    vertical-align: middle;
+  }
+
+  label {
+    margin-left: 0.25rem;
+  }
+}
+
+.filter-option.active label {
+  font-weight: bold;
+  color: red;
+}
+</style>
