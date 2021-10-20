bbac502630e8e090796a913ceb013a7bcd24a2ee
Fabien <cacrfabfr@gmail.com>
2021-10-20 12:14:31 +0200

    coach requests view

--- src/views/requests/RequestsReceived.vue
+++ src/views/requests/RequestsReceived.vue
@@ -1,15 +1,45 @@
 <template>
-  <div>
-    <h2>Requests</h2>
-  </div>
+  <section>
+    <base-card>
+      <header>
+        <h2>Requests Received</h2>
+      </header>
+      <ul v-if="hasRequests">
+        <request-item
+          v-for="req in receivedRequests"
+          :key="req.id"
+          :email="req.userEmail"
+          :message="req.message"
+        ></request-item>
+      </ul>
+      <h3 v-else>You haven't received any requests yet!</h3>
+    </base-card>
+  </section>
 </template>
 
 <script>
-  export default {
-    
-  }
+import RequestItem from "../../components/requests/RequestItem.vue"
+export default {
+  components: {
+    RequestItem,
+  },
+  computed: {
+    receivedRequests() {
+      return this.$store.getters['requests/requests'];
+    },
+    hasRequests() {
+      return this.$store.getters['requests/hasRequests'];
+    }
+  },
+}
 </script>
 
 <style lang="scss" scoped>
+ul {
+  list-style: none;
+  margin: 2rem auto;
+  padding: 0;
+  max-width: 30rem;
+}
 
 </style>
