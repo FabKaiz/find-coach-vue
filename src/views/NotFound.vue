339b4b090580830c7bffe33b2153619ab93150ab
Fabien <cacrfabfr@gmail.com>
2021-10-20 16:13:33 +0200

    added not found page

---
+++ src/views/NotFound.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <base-card>
+      <h1>Page not found</h1>
+      <p>
+        This page could not be found - maybe check out all our
+        <router-link to="/coaches">coaches</router-link>
+      </p>
+    </base-card>
+  </div>
+</template>
