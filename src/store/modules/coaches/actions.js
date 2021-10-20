1628519c68b6faefb637889aaa8f11fd7d2a5db2
Fabien <cacrfabfr@gmail.com>
2021-10-20 15:37:40 +0200

    cathed http response data to auto fetch data

--- src/store/modules/coaches/actions.js
+++ src/store/modules/coaches/actions.js
@@ -27,7 +27,10 @@ export default {
       id: userId,
     });
   },
-  async loadCoaches(context) {
+  async loadCoaches(context, payload) {
+    if (!payload.forceRefresh && !context.getters.shouldUpdate) {
+      return;
+    }
     const response = await fetch(
       `https://find-coach-c96a2-default-rtdb.europe-west1.firebasedatabase.app/coaches.json`
     );
@@ -50,5 +53,6 @@ export default {
       coaches.push(coach);
     }
     context.commit('setCoaches', coaches);
+    context.commit('setFetchTimestamp');
   },
 };
