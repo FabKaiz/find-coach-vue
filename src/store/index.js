c21c294e1387e8a693f75d4a611aa6218152463f
Fabien <cacrfabfr@gmail.com>
2021-10-21 11:47:28 +0200

    auth setup

--- src/store/index.js
+++ src/store/index.js
@@ -2,22 +2,14 @@ import { createStore } from 'vuex';
 
 import coachesModule from './modules/coaches/index.js';
 import requestsModule from './modules/requests/index.js';
+import authModule from './modules/auth/index.js';
 
 export default createStore({
-  state() {
-    return {
-      userId: 'c4',
-    };
-  },
   mutations: {},
   actions: {},
   modules: {
     coaches: coachesModule,
     requests: requestsModule,
-  },
-  getters: {
-    userId(state) {
-      return state.userId;
-    },
+    auth: authModule,
   },
 });
